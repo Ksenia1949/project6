d44434e9c121728f91d75a013d6f217686180ad3
Ksenia <KseniaNovikova@Mila.local>
2020-08-03 23:41:45 -0400

    added code on line : 6 in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TodoList from "./TodoList";
 
 const initialList = [
     {id: 1, title: "first Todo", done: false},
+    {id: 1, title: "first Todo", done: false},
     {id: 1, title: "second Todo", done: false},
     {id: 1, title: "third Todo", done: false}
     ]
